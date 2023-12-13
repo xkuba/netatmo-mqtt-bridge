a70031249188a553e4e24ae79a7211075a9943d6
xkuba <kuba@xkuba.net>
2023-12-13 22:45:31 +0100

    zmena API getstationsdata -> homestatus

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,11 @@
-FROM node:lts-alpine
+FROM --platform=linux/amd64 node:lts-alpine
 ENV NODE_ENV=production
 WORKDIR /usr/src/app
 COPY ["package.json", "package-lock.json*", "npm-shrinkwrap.json*", "./"]
 RUN npm install --production --silent && mv node_modules ../
 COPY . .
 RUN chown -R node app.js
-RUN touch last_refreshtoken.txt
-RUN chown -R node last_refreshtoken.txt
+RUN touch last_accesstoken.txt
+RUN chown -R node last_accesstoken.txt
 USER node
 CMD ["npm", "start"]
--- app.js
+++ app.js
@@ -25,13 +25,13 @@ const log = (message, level) => {
 }
 
 const expiresInSec = 10000
-const refreshTokenFile = "last_refreshtoken.txt";
+const accessTokenFile = "last_accesstoken.txt";
 
 let mqttTopicPrefix = "netatmo";
 let baseUrl = "https://api.netatmo.com"
-let publishIntervalSeconds = 300;
+let publishIntervalSeconds = 30;
 
-const requiredEnvs = ["CLIENT_ID", "CLIENT_SECRET", "REFRESH_TOKEN", "MQTT_HOST"]
+const requiredEnvs = ["CLIENT_ID", "CLIENT_SECRET", "REFRESH_TOKEN", "HOME_ID", "MQTT_HOST"]
 
 requiredEnvs.forEach(env => {
   if (process.env[env] === undefined) {
@@ -51,29 +51,30 @@ if (process.env.MQTT_TOPIC_PREFIX !== undefined) {
   mqttTopicPrefix = process.env.MQTT_TOPIC_PREFIX
 }
 
+let homeId = process.env.HOME_ID;
 let clientId = process.env.CLIENT_ID;
 let clientSecret = process.env.CLIENT_SECRET;
-let accessToken = process.env.ACCESS_TOKEN;
+let accessToken = "";
 let refreshToken = process.env.REFRESH_TOKEN;
-
-if (fs.existsSync(refreshTokenFile)) {
-  fs.stat(refreshTokenFile, (error, stats) => {
-
-    if (error) {
-      log(`Error while accessing ${refreshTokenFile}: ${error}`, 'error');
-    }
-    if (date.addSeconds(stats.mtime, expiresInSec) < new Date()) {
-      const fileContent = fs.readFileSync(refreshTokenFile);
-      if (fileContent !== null && fileContent !== undefined && stats.size > 0) {
-        log("Using local stored refreshToken", 'info');
-        refreshToken = fileContent;
-      }
-    }
-  });
-}
 
 let expireDate = new Date();
 let hasError = false;
+
+/*
+if (fs.existsSync(accessTokenFile)) {
+  fs.stat(accessTokenFile, (error, stats) => {
+    if (error) {
+      log(`Error while accessing ${accessTokenFile}: ${error}`, 'error');
+    }
+    if (date.addSeconds(stats.mtime, expiresInSec) < new Date()) {
+      const fileContent = fs.readFileSync(accessTokenFile);
+      if (fileContent !== null && fileContent !== undefined && stats.size > 0) {
+        log("Using local stored accessToken", 'info');
+        accessToken = fileContent;
+      }
+    }
+  });
+}*/
 
 let mqttOptions = {
   clientId: "netatmobridge",
@@ -112,42 +113,200 @@ mqttClient.on("reconnectnd", () => {
   log(`Reconnecting  to MQTT broker ${process.env.MQTT_HOST}`, 'info');
 });
 
-const convertToMQTT = (data) => {
+const NAModuleType_Name = (code) => {
+  switch (code) {
+	//hlavni stanice
+	case 'NAMain': return "Smart Home Weather station"
+	//venkovni modul
+	case 'NAModule1': return "Smart Outdoor Module"
+	//anometr
+	case 'NAModule2': return "Smart Anemometer Module"
+	//srazkovy sensor
+	case 'NAModule3': return "Smart Rain Gauge Module"
+	//domaci sensor
+	case 'NAModule4': return "Smart Indoor Module"
+	default: return code
+  }
+}
+const NAModuleType_ShortName = (code) => {
+  switch (code) {
+	//hlavni stanice
+	case 'NAMain': return `netatmo_main`
+	//venkovni modul
+	case 'NAModule1': return `netatmo_outdoor`
+	//anometr
+	case 'NAModule2': return `netatmo_anemometer`
+	//srazkovy sensor
+	case 'NAModule3': return `netatmo_rain`
+	//domaci sensor
+	case 'NAModule4': return `netatmo_indoor`
+	default: return code
+  }
+}
 
+const config_diagnostic = (diagnostic) => {
+  if(diagnostic) {
+    return `"entity_category": "diagnostic","enabled_by_default":false,`
+    //return `"entity_category": "diagnostic",`
+  } else {
+    return ``
+  }
+}
+const config_icon = (icon) => {
+  if(icon != "") {
+    return `"icon": "${icon}",`
+  } else {
+    return ``
+  }
+}
+const config_device_class = (device_class) => {
+  if(device_class != "") {
+    return `"device_class": "${device_class}",`
+  } else {
+    return ``
+  }
+}
+  
+const ha_config_publish = (topicConfig, topicState, module, value_name) => {
+  let id = module.id.replaceAll(":", "")
+  var device_class = ""
+  var unit = ""
+  var icon = ""
+  var name = ""
+
+  if (value_name == "temperature"     ) { device_class = "temperature"   ; unit = "\u00b0C"; name = "Teplota"} 
+  else if (value_name == "humidity"   ) { device_class = "humidity"      ; unit = "%"      ; name = "Vlhkost"} 
+  else if (value_name == "co2"        ) { device_class = "carbon_dioxide"; unit = "ppm"    ; name = "CO2"} 
+  else if (value_name == "noise"      ) { icon = "mdi:ear-hearing"       ; unit = "dB"     ; name = "Hluk"} 
+  else if (value_name == "pressure"   ) { device_class = "pressure"      ; unit = "hPa"   ; name = "Tlak"} 
+  else if (value_name == "absolute_pressure") { device_class = "pressure"; unit = "hPa"   ; name = "Tlak absolutní"} 
+
+  else if (value_name == "rain"       ) { icon = "mdi:weather-rainy"; unit = "mm"; name = "Stážky"} 
+  else if (value_name == "sum_rain_1" ) { icon = "mdi:weather-rainy"; unit = "mm"; name = "Stážky hodina"} 
+  else if (value_name == "sum_rain_24") { icon = "mdi:weather-rainy"; unit = "mm"; name = "Stážky dnes"} 
+
+  else if (value_name == "wind_strength"  ) { device_class = "wind_speed" ; unit = "m/s"   ; name = "Síla větru"} 
+  else if (value_name == "wind_angle"     ) { icon = "mdi:compass-outline"; unit = "\u00b0"; name = "Úhel větru"} 
+  else if (value_name == "wind_gust"      ) { device_class = "wind_speed" ; unit = "m/s"   ; name = "Poryv větru"} 
+  else if (value_name == "wind_gust_angle") { icon = "mdi:compass-outline"; unit = "\u00b0"; name = "Úhel poryvu"} 
+
+  else if (value_name == "battery_state") { icon = "mdi:battery"; unit = ""; name = "Baterie"} 
+ 
+  let diagnostic_valueName = ["wifi_strength", "wifi_state", "battery_state", "battery_level", "rf_state", "rf_strength", "reachable"]
+  let diagnostic = diagnostic_valueName.some(v => value_name.includes(v))
+  
+  let msg = `{`+
+    `"state_topic": "${topicState}",`+
+    config_device_class(device_class)+ 
+    config_icon(icon)+
+    config_diagnostic(diagnostic)+
+    `"name": "${name}",`+
+    `"unit_of_measurement": "${unit}",`+ 
+    `"value_template": "{{ value_json.${value_name} }}",`+ 
+    `"unique_id": "${NAModuleType_ShortName(module.type)}_${id}_${value_name}",`+  //opravdovy unikat
+    `"object_id": "${NAModuleType_ShortName(module.type)}_${value_name}",`+  //entity_id
+    `"device": {`+
+      `"name": "Netatmo ${NAModuleType_Name(module.type)}",`+
+      `"identifiers": "${id}",`+
+      `"model": "${NAModuleType_Name(module.type)}",`+
+      `"manufacturer": "Netatmo",`+
+      `"sw_version":"${module.firmware_revision.toString()}"`+
+    `},`+
+    `"platform": "mqtt"`+
+  `}`
+
+  mqttClient.publish(`${topicConfig}${value_name}/config`, msg);
+}
+
+const convertToMQTT = (data) => {
   if (data.status === "ok") {
 
-    data.body.devices.forEach((device, index) => {
+    var home = data.body.home
 
-      let deviceTopic = `${mqttTopicPrefix}/${device._id.replaceAll(":", "")}/`
+    let deviceTopicState = `${mqttTopicPrefix}/${home.id}/`
+    let deviceTopicConfig = `homeassistant/sensor/${home.id}/`
+    
+    if(!home.hasOwnProperty("modules")) {
+      log("No json property 'modules'", 'info');
+      return;
+    }
 
-      if (mqttClient.connected) {
+    if (mqttClient.connected) {
+      log("Publishing device data via mqtt", 'info')
+      
+      home.modules.forEach(module => {
+        let moduleId = module.id.replaceAll(":", "")
+        let topicState = `${deviceTopicState}${moduleId}/state`
+        let topicConfig = `${deviceTopicConfig}${moduleId}_`
+        
+        if(module.type == "NAMain") {
+          ha_config_publish(topicConfig, topicState, module, "wifi_state");
+          ha_config_publish(topicConfig, topicState, module, "wifi_strength");
+        } else {
+          ha_config_publish(topicConfig, topicState, module, "battery_state");
+          ha_config_publish(topicConfig, topicState, module, "battery_level");
+          ha_config_publish(topicConfig, topicState, module, "rf_state");
+          ha_config_publish(topicConfig, topicState, module, "rf_strength");
+          ha_config_publish(topicConfig, topicState, module, "reachable");
+        }
+        
+        switch (module.type) {
+          //hlavni stanice
+          case 'NAMain':  
+            ha_config_publish(topicConfig, topicState, module, "temperature");
+            ha_config_publish(topicConfig, topicState, module, "humidity");
+            ha_config_publish(topicConfig, topicState, module, "co2");
+            ha_config_publish(topicConfig, topicState, module, "noise");
+            ha_config_publish(topicConfig, topicState, module, "pressure");
+            ha_config_publish(topicConfig, topicState, module, "absolute_pressure");
+            break;
+          
+            //venkovni modul
+          case 'NAModule1':   
+            ha_config_publish(topicConfig, topicState, module, "temperature");
+            ha_config_publish(topicConfig, topicState, module, "humidity");
+            break;
 
-        log("Publishing device data via mqtt", 'info')
+          //anometr
+          case 'NAModule2':   
+            //prevedu na m/s a zaokrouhlim na 2 des. mista
+            module.wind_strength = Math.round(module.wind_strength / 3.6 * 100)/100;
+            module.wind_gust = Math.round(module.wind_gust / 3.6 * 100)/100; 
 
-        mqttClient.publish(`${deviceTopic}type`, device.type.toString());
-        mqttClient.publish(`${deviceTopic}Temperature`, device.dashboard_data.Temperature.toString());
-        mqttClient.publish(`${deviceTopic}CO2`, device.dashboard_data.CO2.toString());
-        mqttClient.publish(`${deviceTopic}Humidity`, device.dashboard_data.Humidity.toString());
-        mqttClient.publish(`${deviceTopic}Noise`, device.dashboard_data.Noise.toString());
-        mqttClient.publish(`${deviceTopic}Pressure`, device.dashboard_data.Pressure.toString());
+            ha_config_publish(topicConfig, topicState, module, "wind_strength");
+            ha_config_publish(topicConfig, topicState, module, "wind_angle");
+            ha_config_publish(topicConfig, topicState, module, "wind_gust");
+            ha_config_publish(topicConfig, topicState, module, "wind_gust_angle");
+            break;
 
-        device.modules.forEach(module => {
+          //srazkovy sensor
+          case 'NAModule3':   
+            ha_config_publish(topicConfig, topicState, module, "rain");
+            ha_config_publish(topicConfig, topicState, module, "sum_rain_1");
+            ha_config_publish(topicConfig, topicState, module, "sum_rain_24");
+            break;
 
-          let moduleTopic = `${deviceTopic}modules/${module._id.replaceAll(":", "")}/`
+          //domaci sensor
+          case 'NAModule4':   
+            ha_config_publish(topicConfig, topicState, module, "temperature");
+            ha_config_publish(topicConfig, topicState, module, "humidity");
+            ha_config_publish(topicConfig, topicState, module, "co2");
+            break;
 
-          mqttClient.publish(`${moduleTopic}type`, module.type);
-          mqttClient.publish(`${moduleTopic}battery_percent`, module.battery_percent.toString());
-          mqttClient.publish(`${moduleTopic}Temperature`, module.dashboard_data.Temperature.toString());
-          mqttClient.publish(`${moduleTopic}Humidity`, module.dashboard_data.Humidity.toString());
-        });
-      }
-    });
+          default:
+            break;  
+        }
+            
+        mqttClient.publish(topicState, JSON.stringify(module));
+      });
+    }
   }
 }
 
 const doTokenRefresh = () => {
-
-  if (expireDate < new Date()) {
+//log("1", 'info')
+  if (expireDate < new Date() || accessToken == "") {
+//log("11", 'info')
     request.post(
       {
         url: `${baseUrl}/oauth2/token`,
@@ -160,13 +319,17 @@ const doTokenRefresh = () => {
       },
       (error, response, body) => {
         if (!error && response.statusCode == 200) {
+//log("12", 'info')
           const jsonResult = JSON.parse(body);
+          accessToken = jsonResult.access_token;
           refreshToken = jsonResult.refresh_token;
           expireDate = date.addSeconds(new Date(), jsonResult.expires_in - 800);
-          fs.writeFileSync(refreshTokenFile, jsonResult.refresh_token);
-          log(`Updated netatmo api refresh token and saved it under ${refreshTokenFile}`, 'info')
+          
+          fs.writeFileSync(accessTokenFile, accessToken);
+          log(`Updated netatmo api refresh token and saved it under ${accessTokenFile}`, 'info')
         }
         else {
+//log("13", 'info')
           hasError = true;
           log(JSON.stringify(response, null, 3), "warn");
           log(error, "error");
@@ -176,40 +339,60 @@ const doTokenRefresh = () => {
   }
 }
 
-
-const getStationData = () => {
-
+const getHomeStatus = () => {
+//log("0", 'info')
   doTokenRefresh();
-
-  request.get(`${baseUrl}/api/getstationsdata?get_favorites=false`,
-    {
-      headers: {
-        "Authorization": `Bearer ${accessToken}`
+  
+  if(accessToken != "") {
+//log("01", 'info')
+    request.get(`${baseUrl}/api/homestatus?home_id=${homeId}`,
+      {
+        headers: {
+          "Authorization": `Bearer ${accessToken}`
+        }
+      }, (error, response, body) => {
+        if (!error && response.statusCode == 200) {
+//log("02", 'info')
+          const jsonResult = JSON.parse(body);
+          convertToMQTT(jsonResult);
+        } else {
+//log("03", 'info')
+          hasError = true;
+          log(JSON.stringify(response, null, 3), "warn");
+          log(error, "error");
+//          mqttClient.end(true);
+//          process.exit(1);
+        }
       }
-    }, (error, response, body) => {
-      if (!error && response.statusCode == 200) {
-        const jsonResult = JSON.parse(body);
-        convertToMQTT(jsonResult);
-      } else {
-        hasError = true;
-        log(JSON.stringify(response, null, 3), "warn");
-        log(error, "error");
-        mqttClient.end(true);
-        process.exit(1);
-      }
-    });
+    );
+  } else {
+//log("04", 'info')
+    hasError = true;
+    log(`accessToken EMPTY`, 'warn')
+  }
 }
 
 const publishNetatmo = () => {
   if (hasError) {
-    log("Stopping proccess.", "error");
-    mqttClient.end(true);
-    process.exit(1);
-  }
-  else {
+    log("publishNetatmo: hasError=true", "error");
+    
+//    mqttClient.end(true);
+//    process.exit(1);
+
+    //pokud dojde k chybe komunikace, pockam 10s a zkusim znovu
+    hasError = false;
+    return Promise.delay(10 * 1000).then(() => publishNetatmo());
+  
+  } else {
     log("Getting station data", 'info')
-    getStationData();
-    return Promise.delay(publishIntervalSeconds * 1000).then(() => publishNetatmo());
+    getHomeStatus();
+    
+    if (hasError) {
+      //pokud dojde k chybe komunikace, pockam 10s a zkusim znovu
+      return Promise.delay(10 * 1000).then(() => publishNetatmo());
+    } else {
+      return Promise.delay(publishIntervalSeconds * 1000).then(() => publishNetatmo());
+    }
   }
 }
 
